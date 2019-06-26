93d24009df1e44b63d1667db6fb776a32a4033ef
IRIN <irina.ulida@yandex.ru>
2019-06-26 20:08:53 +0300

    WorkOfRegex

--- WorkOfText/WorkOfText/Program.cs
+++ WorkOfText/WorkOfText/Program.cs
@@ -12,51 +12,51 @@ namespace WorkOfText
     {
         static void Main(string[] args)
         {
-
-            StreamReader str = new StreamReader(@"C:\C sharp\WorkOfText\WorkOfText\bin\Debug\text.txt");
-            var s = str.ReadToEnd(); 
+            string text;
+            using (StreamReader str = new StreamReader(@"C:\C sharp\WorkOfText\WorkOfText\bin\Debug\text.txt"))
+            {
+                text = str.ReadToEnd();
+            }
+                                    
+            string patternPuntuation = @"[[\](){}<>*_-\\]";
+            var punctuation1 = Regex.Replace (text, patternPuntuation, "");
+            Console.WriteLine(text);
            
 
-            
-            using (StreamReader punctuation = new StreamReader(s)) 
+            using (StreamReader Outputwords = new StreamReader(text))  
             {
-                string patternPuntuation = @"[[\](){}<>*_-]";
-                var punctuation1 = Regex.Replace (s,patternPuntuation ," ");
-                Console.WriteLine(s);
-            };
-
-            using (StreamReader words = new StreamReader(s))  
-            {
-                Regex word = new Regex(@"\b.*\b",RegexOptions.IgnoreCase);
-                if (word.IsMatch(s))
+                Regex word = new Regex(@"\b\w*\b",RegexOptions.IgnoreCase);
+                if (word.IsMatch(text))
                 {
-                    Console.WriteLine(word.Matches(s));
+                    Console.WriteLine(word.Matches(text));
+                    
+                    
                 }
-                if (!word.IsMatch(s))
+                if (!word.IsMatch(text))
                 {
                     Console.WriteLine("Exception");
                 }
             };
 
-            using (StreamReader sentences = new StreamReader(s))  
+            using (StreamReader sentences = new StreamReader(text))  
             {
                 Regex sentence = new Regex(@"[A-Za-z]+[^.!?]*[.!?]", RegexOptions.IgnoreCase);
-                if (sentence.IsMatch(s))
+                if (sentence.IsMatch(text))
                 {
-                    Console.WriteLine(sentence.Matches(s));
+                    Console.WriteLine(sentence.Matches(text));
                 }
-                if (!sentence.IsMatch(s))
+                if (!sentence.IsMatch(text))
                 {
                     Console.WriteLine("Exception");
                 }
                 
             };
 
-            using (StreamReader Wordcount = new StreamReader(s))
+            using (StreamReader Wordcount = new StreamReader(text))
             {
-                string[] inputSentence =s.Split();
+                string[] inputSentence = text.Split();
                 int count = 0;
-                string pattern = @"(?:\b\w+\ \s|\S)*" + s + @"(?:\b\w+\b\ \s|\S)?";
+                string pattern = @"(?:\b\w+\ \s|\S)*" + text + @"(?:\b\w+\b\ \s|\S)?";
                 Regex rx = new Regex(pattern, RegexOptions.IgnoreCase);
                 for (int i = 0; i < inputSentence.Length; i++)
                 {
@@ -66,7 +66,7 @@ namespace WorkOfText
 
             };
           
-            str.Close();
+            
             Console.ReadLine();
 
 
